ff54e4b878d525b76656108a8a1689a5d3b54f92
Marta <marta@Martas-MacBook-Air.local>
2018-11-03 17:21:55 +0200

    Add blog posts to front page

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mm&mj9^5-$hsttmxis8i02ec+jl-t$9*%jh_@iv-)*@(!4eyis'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
